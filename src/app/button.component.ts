5fe1a278a35773706536edf81ecbe2dfc85f8d29
Mark Smyda <mark.smyda@gmail.com>
2025-05-25 00:00:26 -0400

    Display papers from result set

--- src/app/button.component.ts
+++ src/app/button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { CoreServiceService } from './core-service.service';
-import { CoreInterface } from './core-interface';
+import { CoreInterface, Result } from './core-interface';
 
 @Component({
     selector: 'button-demo',
@@ -11,19 +11,31 @@ import { CoreInterface } from './core-interface';
     imports: [ButtonModule, TableModule]
 })
 export class ButtonDemo {
-    products = [
-        { code: 'P101', name: 'Product 1', category: 'Electronics', quantity: 10 },
-        { code: 'P102', name: 'Product 2', category: 'Clothing', quantity: 20 }
-    ];
-
-    data: CoreInterface | undefined;
+    data: CoreInterface = {
+        totalHits: 0,
+        limit: 0,
+        offset: 0,
+        results: [],
+        searchId: ''
+    };
 
     constructor(private coreService: CoreServiceService) {}
 
     fetch() {
-        this.coreService.getCoreData('(drone)').subscribe((data) => {
-            this.data = data;
-            console.log(this.data);
+        this.coreService.getCoreData('(drone)').subscribe({
+            next: (data) => {
+                this.data = data;
+                console.log(this.data);
+            },
+            error: (error) => {
+                console.log('Problem contacting service: ', error);
+                throw new Error('Failed to fetch Core data.');
+            }
         });
     }
+
+    getAuthorNames(result: Result): string {
+        return result.authors.map(author => author.name).join(', ');
+    }
+
 }
